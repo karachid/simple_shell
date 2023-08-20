4e394be0085b0bdf7a388a79ee3bb9f2e330e6c2
Anasoud1 <anasoudsassi@gmail.com>
2023-08-20 07:35:21 -0700

    adding exit functions file

---
+++ exit_functions.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+void handleexitcommand(char **tokens, list_t *head)
+{
+	int num = 0;
+
+	if (tokens[1])
+		num = atoi(tokens[1]);
+	freelist(head);
+	freearray(tokens);
+	exit(num);
+}
